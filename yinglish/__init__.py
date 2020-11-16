e417ba1ae75d96c34c6560dec0a1bbfd10bef3cb
chenmaster05 <38460778+chenmaster05@users.noreply.github.com>
2020-11-16 16:44:15 +1100

    更色了！
    ```py
    import yinglish
    
    s = '吃葡萄不吐葡萄皮。'
    print(yinglish.chs2yin(s))
    # ……吃〇〇不吐葡……葡萄皮……
    ```

--- yinglish/__init__.py
+++ yinglish/__init__.py
@@ -1,10 +1,11 @@
 import random
 
 import jieba
+import jieba.posseg as np # 惊！？
 jieba.setLogLevel(20)
 
 
-def _词转换(x, 淫乱度):
+def _词转换(x, y, 淫乱度):
     if random.random() > 淫乱度:
         return x
     if x in {'，', '。'}:
@@ -12,11 +13,13 @@ def _词转换(x, 淫乱度):
     if len(x) > 1 and random.random() < 0.5:
         return f'{x[0]}……{x}'
     else:
+        if y == 'n' and random.random() < 0.5:
+            x = ''.join(['〇' for _ in x])
         return f'……{x}'
 
 
 def chs2yin(s, 淫乱度=0.5):
-    return ''.join([_词转换(x, 淫乱度) for x in jieba.cut(s)])
+    return ''.join([_词转换(x, y, 淫乱度) for x, y in np.cut(s)])
 
 
 if __name__ == '__main__':
