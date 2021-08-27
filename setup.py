fc475d6ce734704ecca70f0474e3e0a831590d3d
黄巍 <RimoChan@stu.xmu.edu.cn>
2021-08-28 00:36:07 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='yinglish',
-    version='0.0.1',
+    version='1.0.0',
     author='RimoChan',
     author_email='the@librian.net',
     description='yinglish',
